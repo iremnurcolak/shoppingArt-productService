c0766efd36adb0d941eb4dd0cd9a5f47e13511f9
iremnurcolak <iremnurcolak34@gmail.com>
2023-07-31 23:28:39 +0300

    Initial commit

---
+++ src/main/java/com/lobemus/shoppingAppproductService/ShoppingAppProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lobemus.shoppingAppproductService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingAppProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingAppProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lobemus/shoppingAppproductService/ShoppingAppProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lobemus.shoppingAppproductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAppProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
