6505866e01ecd2bcab2a5a0957c9ca486cc3ef2b
iremnurcolak <iremnurcolak34@gmail.com>
2023-08-01 12:26:42 +0300

    Added integration tests for createProduct

--- src/test/java/com/lobemus/shoppingAppproductService/ShoppingAppProductServiceApplicationTests.java
+++ src/test/java/com/lobemus/shoppingAppproductService/ShoppingAppProductServiceApplicationTests.java
@@ -1,13 +1,74 @@
 package com.lobemus.shoppingAppproductService;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lobemus.shoppingAppproductService.dto.ProductRequest;
+import com.lobemus.shoppingAppproductService.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ShoppingAppProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
+	@Autowired
+	private ProductRepository productRepository;
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry)
+	{
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri",
+				mongoDBContainer::getReplicaSetUrl);
 	}
 
+	@Test
+	void testCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		String productReqString = objectMapper.writeValueAsString(productRequest);
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(productReqString))
+				.andExpect(status().isCreated());
+
+        Assertions.assertEquals(1, productRepository.findAll().size());
+	}
+
+	//TODO
+	/*
+	@Test
+	void testGetAllProducts()
+	{
+
+	}
+	*/
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("iPhone13")
+				.description("iPhone13")
+				.price(BigDecimal.valueOf(67455))
+				.build();
+	}
 }
